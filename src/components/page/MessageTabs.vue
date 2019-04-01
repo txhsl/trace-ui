7e700b48037d95e9591293ce2a4ba2a0195f7f37
txhsl <799498265@qq.com>
2019-04-02 01:46:53 +0800

    message part1

--- src/components/page/MessageTabs.vue
+++ src/components/page/MessageTabs.vue
@@ -2,7 +2,7 @@
     <div class="">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-copy"></i> tab选项卡</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-lx-copy"></i> 消息中心</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
@@ -14,54 +14,59 @@
                                 <span class="message-title">{{scope.row.title}}</span>
                             </template>
                         </el-table-column>
-                        <el-table-column prop="date" width="180"></el-table-column>
-                        <el-table-column width="120">
+                        <el-table-column prop="target"></el-table-column>
+                        <el-table-column prop="date" width="150"></el-table-column>
+                        <el-table-column width="180">
                             <template slot-scope="scope">
-                                <el-button size="small" @click="handleRead(scope.$index)">标为已读</el-button>
+                                <el-button type="primary" size="small" @click="handleAccept(scope.$index)">接受</el-button>
+                                <el-button size="small" @click="handleIgnore(scope.$index)">忽略</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
                     <div class="handle-row">
-                        <el-button type="primary">全部标为已读</el-button>
+                        <el-button type="primary" @click="handleIgnoreAll()">全部忽略</el-button>
                     </div>
                 </el-tab-pane>
-                <el-tab-pane :label="`已读消息(${read.length})`" name="second">
+                <el-tab-pane :label="`已接受消息(${accepted.length})`" name="second">
                     <template v-if="message === 'second'">
-                        <el-table :data="read" :show-header="false" style="width: 100%">
+                        <el-table :data="accepted" :show-header="false" style="width: 100%">
                             <el-table-column>
                                 <template slot-scope="scope">
                                     <span class="message-title">{{scope.row.title}}</span>
                                 </template>
                             </el-table-column>
+                            <el-table-column prop="target"></el-table-column>
                             <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
+                            <el-table-column width="180">
                                 <template slot-scope="scope">
-                                    <el-button type="danger" @click="handleDel(scope.$index)">删除</el-button>
+                                    <el-button type="danger" @click="handleDelAcp(scope.$index)">删除</el-button>
                                 </template>
                             </el-table-column>
                         </el-table>
                         <div class="handle-row">
-                            <el-button type="danger">删除全部</el-button>
+                            <el-button type="danger" @click="handleDeleteAll()">删除全部</el-button>
                         </div>
                     </template>
                 </el-tab-pane>
-                <el-tab-pane :label="`回收站(${recycle.length})`" name="third">
+                <el-tab-pane :label="`已忽略请求(${ignored.length})`" name="third">
                     <template v-if="message === 'third'">
-                        <el-table :data="recycle" :show-header="false" style="width: 100%">
+                        <el-table :data="ignored" :show-header="false" style="width: 100%">
                             <el-table-column>
                                 <template slot-scope="scope">
                                     <span class="message-title">{{scope.row.title}}</span>
                                 </template>
                             </el-table-column>
+                            <el-table-column prop="target"></el-table-column>
                             <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
+                            <el-table-column width="180">
                                 <template slot-scope="scope">
-                                    <el-button @click="handleRestore(scope.$index)">还原</el-button>
+                                    <el-button type="primary" size="small" @click="handleAcceptIgn(scope.$index)">接受</el-button>
+                                    <el-button @click="handleDelIgn(scope.$index)">删除</el-button>
                                 </template>
                             </el-table-column>
                         </el-table>
                         <div class="handle-row">
-                            <el-button type="danger">清空回收站</el-button>
+                            <el-button type="danger" @click="handleDeleteAllIgn()">删除全部</el-button>
                         </div>
                     </template>
                 </el-tab-pane>
@@ -79,35 +84,56 @@
                 showHeader: false,
                 unread: [{
                     date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护',
-                },{
-                    date: '2018-04-19 21:00:00',
-                    title: '今晚12点整发大红包，先到先得',
+                    target: '0x23f9ec3e3916a8f04495165d7cdbe14eb5da5bee',
+                    title: 'Producer_Operator',
                 }],
-                read: [{
+                accepted: [{
                     date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
+                    target: '0x392f19458deb6872fc3ed4f4157fb1ef173c8149',
+                    title: 'Producer_Operator'
                 }],
-                recycle: [{
+                ignored: [{
                     date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
+                    target: '0x53888073d8cd1350b939e7f0e0776defebbe3a0a',
+                    title: 'Producer_Operator'
                 }]
             }
         },
         methods: {
-            handleRead(index) {
+            getMessages() {
+
+            },
+            handleAccept(index) {
                 const item = this.unread.splice(index, 1);
-                console.log(item);
-                this.read = item.concat(this.read);
+                this.accepted = this.accepted.concat(item);
             },
-            handleDel(index) {
-                const item = this.read.splice(index, 1);
-                this.recycle = item.concat(this.recycle);
+            handleAcceptIgn(index) {
+                const item = this.ignored.splice(index, 1);
+                this.accepted = this.accepted.concat(item);
             },
-            handleRestore(index) {
-                const item = this.recycle.splice(index, 1);
-                this.read = item.concat(this.read);
+            handleIgnore(index) {
+                const item = this.unread.splice(index, 1);
+                this.ignored = this.ignored.concat(item);
+            },
+            handleDelAcp(index) {
+                const item = this.accepted.splice(index, 1);
+            },
+            handleDelIgn(index) {
+                const item = this.ignored.splice(index, 1);
+            },
+            handleDeleteAll() {
+                this.accepted = [];
+            },
+            handleIgnoreAll() {
+                this.ignored = this.ignored.concat(this.unread);
+                this.unread = [];
+            },
+            handleDeleteAllIgn() {
+                this.ignored = [];
             }
+        },
+        created() {
+            this.getMessages();
         },
         computed: {
             unreadNum(){
